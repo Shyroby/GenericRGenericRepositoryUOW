e903ca26432794f6a80da041e9bd8c14395d1e77
Marco Picasso <marco.picasso@hotmail.it>
2018-11-24 16:24:46 +0100

    Cleaning code

--- GenericRepositoryDemo/Models/DocumentsDbContext.cs
+++ GenericRepositoryDemo/Models/DocumentsDbContext.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GenericRepositoryDemo.Models
 {
-    
+
 
     public class DocumentsDbContext : DbContext
     {
--- GenericRepositoryDemo/Startup.cs
+++ GenericRepositoryDemo/Startup.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GenericRepositoryDemo.Models;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace GenericRepositoryDemo
 {
@@ -27,9 +18,9 @@ namespace GenericRepositoryDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //string connection = Configuration.GetConnectionString("DefaultConnection");
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            //services.AddDbContext<DocumentsDbContext>(options => options.UseSqlite(connection));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
